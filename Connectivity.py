5c8c8a19fc93dd79d9b6ea1566e75c38db95d91d
gerlock93 <39182996+gerlock93@users.noreply.github.com>
2020-08-05 17:37:09 +0200

    Update Connectivity.py
    ora dovrebbe essere tutto ok, va solo ordinato e ripulito, rinominate le variabili e messi i commenti

--- Connectivity.py
+++ Connectivity.py
@@ -1,4 +1,9 @@
 import networkx as nx
+import operator
+from collections import OrderedDict
+import itertools
+import collections
+
 
 class Connectivity:
 
@@ -7,54 +12,112 @@ class Connectivity:
 
     def connected_component(self, G):
         CCGraphs = list(nx.connected_components(G))
-        sortedCC = sorted(CCGraphs, key=nx.density, reverse=True)
-        return sortedCC
+        CCGraphs.sort(key=len, reverse=True)
+        return CCGraphs
+        # sortedCC = sorted(CCGraphs, key=nx.density, reverse=True)
+
+
 
     def get_topTenBrokers(self, G):
         """
         :param G: Undirected graph
         :return: A dictionary that contains for each CC of G the top 10 brokers in the CC and the relative score.
-        """
-        d = self.connected_component(G)
-        topten = d[:10]
-        dicX = {}
-        for k in topten:
-            dicBcTen = nx.betweenness_centrality(k)
-            sortedDicBcTen = sorted(dicBcTen, key=dicBcTen.get, reverse=True)
-            dicX[k] = sortedDicBcTen
-        return dicX
+        Find the top 10 brokers in the top 10 most dense connected components."""
 
-    def get_topTwentyBrokers(self, G):
-        """
+        toptenCC = self.get_topTenDenseCC(G)
 
-        :param G: Undirected graph
-        :return:  A dictionary that contains for the CC of G the top 10 brokers in the CC and the relative score.
-        """
-        d = self.connected_component(G)
-        topDense = d[0]
-        dicY = {}
-        for k in topDense:
-            dicBcTwenty = nx.betweenness_centrality(k)
-            sortedDicBcTwenty = sorted(dicBcTwenty.values())
-            dicY[k] = sortedDicBcTwenty
-        return dicY
+        toptenBro = {}
+        for k in toptenCC:
+            S = G.subgraph(k).copy()
+            dicBcTen = nx.betweenness_centrality(S)
+            sortedDicBcTen = OrderedDict(sorted(dicBcTen.items(), key=lambda x: x[1], reverse=True))
+            ii = 0
+            dicX = {}
+            #print("len prima: " + str(len(dicX)))
+            for broker in sortedDicBcTen:
+                ii = ii + 1
+                if ii <= 10:
+                    dicX[broker] = sortedDicBcTen[broker]
+                    #print("bro " + str(broker) + " numero " + str(ii))
+                else:
+                    toptenBro[tuple(k)] = dicX
+                    #print("len dopo: " + str(len(dicX)))
+                    break
+        return toptenBro
 
     def get_topTenDenseCC(self, G):
-        f = open('connectivity.txt', 'w')
         d = self.connected_component(G)
-        topten = d[:10]
+        dizionario = {}
+        topten = {}
         i = 0
-        for k in topten:
+        for k in d:
+            S = G.subgraph(k).copy()
             i = i + 1
-            f = open('connectivity.txt', 'a')
-            f.write(i + " Component: " + nx.nodes(k) + " Density: " + nx.density(k) + "\n")
+            dizionario[tuple(k)] = nx.density(S)
+            print(" Component number:  " + str(i) + " Density: " + str(nx.density(S)) + " contain: " + str(
+                nx.nodes(S)) + "\n")
+        # print(" grandezza dizionario:  " + str(len(dizionario)) + "\n")
+        sortedDict = OrderedDict(sorted(dizionario.items(), key=lambda x: x[1], reverse=True))
+        f = open('connectivity.txt', 'w')
+        i = 0
+        for k in sortedDict:
+            i = i + 1
+            if i <= 10:
+                # print(" Component number:  " + str(i)  + " key: " + str(k) )
+                # print(" value: " + str(sortedDict[k]) + "\n")
+                topten[tuple(k)] = sortedDict.get(k)
+                f.write(" Component number:  " + str(i) + " Density: " + str(sortedDict.get(k)) + " contain: \n")
+            else:
+                break
+
         f.close()
         return topten
 
+    def get_topOneDenseCC(self, G):
+        d = self.connected_component(G)
+        dizionario = {}
+        topOne = {}
+        i = 0
+        for k in d:
+            S = G.subgraph(k).copy()
+            i = i + 1
+            dizionario[tuple(k)] = nx.density(S)
+            print(" Component number:  " + str(i) + " Density: " + str(nx.density(S)) + " contain: " + str(
+                nx.nodes(S)) + "\n")
+        # print(" grandezza dizionario:  " + str(len(dizionario)) + "\n")
+        sortedDict = OrderedDict(sorted(dizionario.items(), key=lambda x: x[1], reverse=True))
+        i = 0
+        for k in sortedDict:
+            i = i + 1
+            if i == 1:
+                # print(" Component number:  " + str(i)  + " key: " + str(k) )
+                # print(" value: " + str(sortedDict[k]) + "\n")
+                topOne[tuple(k)] = sortedDict.get(k)
+                return topOne
+            else:
+                break
+        return topOne
 
-
-
-
-
-
-
+    def get_topTwentyBrokers(self, G):
+        """:param G: Undirected graph
+         :return: A dictionary that contains for each CC of G the top 10 brokers in the CC and the relative score.
+        Find the top 10 brokers in the top 10 most dense connected components."""
+        topOneCC = self.get_topOneDenseCC(G)
+        toptwentyBro = {}
+        for k in topOneCC:
+            S = G.subgraph(k).copy()
+            dicBcTwe = nx.betweenness_centrality(S)
+            sortedDicBcTwe = OrderedDict(sorted(dicBcTwe.items(), key=lambda x: x[1], reverse=True))
+            ii = 0
+            dicX = {}
+            #print("len prima: " + str(len(dicX)))
+            for broker in sortedDicBcTwe:
+                ii = ii + 1
+                if ii <= 20:
+                    dicX[broker] = sortedDicBcTwe[broker]
+                    #print("bro " + str(broker) + " numero " + str(ii))
+                else:
+                    toptwentyBro[tuple(k)] = dicX
+                    #print("len dopo: " + str(len(dicX)))
+                    break
+        return toptwentyBro
