18782ab2dedc0bd9ddecc83138e7ee017b7b3167
angelodimambro <angelweb1995@gmail.com>
2020-09-13 17:39:03 +0200

    delivery version final

--- Main.py
+++ Main.py
@@ -44,25 +44,25 @@ class Main:
         users = UsersReader(filename=checkins, G=G)
 
         # TASK 2a similarities: Print Cosine/Jaccard similarity for each pair of nodes: DONE
-        sim = Similarities(output_dir, users.getMap(), G)
+        Similarities(output_dir, users.getMap(), G)
 
         # TASK 2b: select top 10 dense CC: DONE
         c = Connectivity(G, output_dir)
         topDenseCC = c.getMostDenseCC()[0]
 
         # TASK 2c: find top 10 brokers in top 10 dense CC: DONE
-        top10brokers = c.getTopTenBrokers()
+        c.getTopTenBrokers()
 
         # TASK 2d: from top 20 brokers in the most dense CC apply LTM: DONE
         top20brokers = c.getTopTwentyBrokers()
-        influence = LTM(topDenseCC, top20brokers[1], users.getMap(), output_dir)
+        LTM(topDenseCC, top20brokers[1], users.getMap(), output_dir)
 
         # TASK 3 and 4: create and evaluate the recommender system: DONE
         nodes = []
         for node in G.nodes:
             nodes.append(len(G.edges(node)))
         avg_friends = 1 if is_full else round((sum(nodes) / (len(nodes))) / 2)
-        NN = RecommenderNN(users, G, avg_friends, is_full, data_dir)
+        RecommenderNN(users, G, avg_friends, is_full, data_dir)
 
 
 if __name__ == '__main__':
